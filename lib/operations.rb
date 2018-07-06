7ba72886b597a2cdb5b7e04c334bec7e1d152725
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-07-06 03:29:48 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,12 @@
 def unsafe?(speed)
- speed < 40 || speed > 60
+ if speed < 40 || speed > 60
 end
 
 
 
 def not_safe?(speed)
-	? speed < 40 : "not_safe"
-  ? speed > 60 : "not_safe"
+	? speed < 40 :
+  ? speed > 60 : 
 end
 	
 
